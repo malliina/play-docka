8ac16d12e5f122e6355ee5cc8c4d6ebbdc879df2
Michael Skogberg <michael.skogberg@futurice.com>
2017-01-05 10:54:25 +0200

    Add git tag support

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -5,12 +5,17 @@ import sbt._
 import sbtbuildinfo.BuildInfoPlugin
 import sbtbuildinfo.BuildInfoPlugin.autoImport._
 
+import scala.util.Try
+
 object PlayBuild {
+
+  lazy val gitTag = taskKey[Option[String]]("The git tag of the currently checked out commit, if any")
+
   lazy val p = Project("play-docka", file("."))
     .enablePlugins(BuildInfoPlugin, PlayScala)
     .settings(commonSettings: _*)
 
-  lazy val commonSettings = buildInfoSettings ++ dockaSettings ++ Seq(
+  lazy val commonSettings = gitSettings ++ buildInfoSettings ++ dockaSettings ++ Seq(
     organization := "com.malliina",
     version := "0.0.23",
     scalaVersion := "2.11.8",
@@ -24,6 +29,13 @@ object PlayBuild {
     )
   )
 
+  def gitSettings = Seq(
+    gitTag := {
+      Try(Process("git" :: "describe" :: "--exact-match" :: Nil).lines(NoopLogger()).headOption)
+        .toOption.getOrElse(None)
+    }
+  )
+
   def dockaSettings = dockerSettings ++ Seq(
     codeBuildServiceRole := "codebuild-docka-build-service-role"
   )
@@ -32,4 +44,17 @@ object PlayBuild {
     buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion, sbtVersion),
     buildInfoPackage := "com.malliina.app.build"
   )
+
+  class NoopLogger extends sbt.ProcessLogger {
+    override def info(s: => String) = ()
+
+    override def error(s: => String) = ()
+
+    override def buffer[T](f: => T) = f
+  }
+
+  object NoopLogger {
+    def apply() = new NoopLogger
+  }
+
 }

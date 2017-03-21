ed6dfc88a912878f2b7f7b10fae286838ed4831c
Michael Skogberg <malliina123@gmail.com>
2017-03-21 20:41:56 +0100

    Add resolver

--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -5,6 +5,7 @@ object BuildBuild {
   val settings = sbtPlugins ++ Seq(
     scalaVersion := "2.10.6",
     resolvers ++= Seq(
+      Resolver.bintrayRepo("malliina", "maven"),
       Resolver.url("malliina bintray sbt", url("https://dl.bintray.com/malliina/sbt-plugins/"))(Resolver.ivyStylePatterns)
     ),
     scalacOptions ++= Seq("-unchecked", "-deprecation")

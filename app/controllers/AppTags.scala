0041cb36ec51aa3ee83f933455f6458658e687e3
Michael Skogberg <michael.skogberg@futurice.com>
2018-12-10 11:02:27 +0200

    Add favicon

--- app/controllers/AppTags.scala
+++ app/controllers/AppTags.scala
@@ -1,24 +1,23 @@
 package controllers
 
-import com.malliina.app.build.BuildInfo
+import com.malliina.app.build.BuildInfo.{gitHash, name, version}
 import play.api.mvc.Call
 import scalatags.Text.GenericAttr
 import scalatags.Text.all._
 
 object AppTags {
   implicit val callAttr = new GenericAttr[Call]
-
-  val appName = BuildInfo.name
-  val appVersion = BuildInfo.version
+  val reverse = routes.Home
 
   def index(msg: String) = TagPage(
     html(
       head(
-        link(rel := "stylesheet", href := routes.Home.versioned("css/main.css"))
+        link(rel := "stylesheet", href := reverse.versioned("css/main.css")),
+        link(rel := "shortcut icon", href := reverse.versioned("img/favicon-256.png"))
       ),
       body(
         h2(msg),
-        p(s"$appName $appVersion ${BuildInfo.gitHash}")
+        p(s"$name $version $gitHash")
       )
     )
   )

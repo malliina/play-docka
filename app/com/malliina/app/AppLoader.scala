7cb8fc21c2decfc04e12468b8753b7f1a45a61aa
Michael Skogberg <michael.skogberg@futurice.com>
2018-12-20 17:34:18 +0200

    Add access log

--- app/com/malliina/app/AppLoader.scala
+++ app/com/malliina/app/AppLoader.scala
@@ -3,6 +3,7 @@ package com.malliina.app
 import controllers.{AssetsBuilder, AssetsComponents, Home}
 import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
+import play.api.mvc.EssentialFilter
 import play.api.routing.Router
 import play.filters.HttpFiltersComponents
 import play.filters.gzip.GzipFilter
@@ -15,7 +16,8 @@ class AppComponents(context: Context)
     with HttpFiltersComponents
     with AssetsComponents {
 
-  override val httpFilters = Seq(new GzipFilter(), csrfFilter, securityHeadersFilter)
+  override val httpFilters: Seq[EssentialFilter] =
+    Seq(AccessLogFilter(executionContext), new GzipFilter(), csrfFilter, securityHeadersFilter)
   val as = new AssetsBuilder(httpErrorHandler, assetsMetadata)
   val home = new Home(as, controllerComponents)
   override val router: Router = new Routes(httpErrorHandler, home)
